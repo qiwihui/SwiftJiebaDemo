94852b1357b0a0a4b2e8b92384fbdb1b16c80ed8
qiwihui <qwh005007@gmail.com>
2019-01-15 13:27:29 +0800

    feat: 3. Swift 调用 Objective-C

---
+++ SwiftJiebaDemo/SwiftJiebaDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftJiebaDemo-Bridging-Header.h
+//  SwiftJiebaDemo
+//
+//  Created by Qiwihui on 1/15/19.
+//  Copyright © 2019 Qiwihui. All rights reserved.
+//
+
+#ifndef SwiftJiebaDemo_Bridging_Header_h
+#define SwiftJiebaDemo_Bridging_Header_h
+
+#import "iosjiebaWrapper.h"
+
+#endif /* SwiftJiebaDemo_Bridging_Header_h */
